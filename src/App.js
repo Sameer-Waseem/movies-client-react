e19f8db2d34d24ebe312933b00b5e032cde1ef9b
Sameer M. Waseem <sameerwaseem969@gmail.com>
2021-11-24 19:23:18 +0500

    Root file

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import Customers from "./components/customers";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ToastContainer></ToastContainer>
+        <NavBar></NavBar>
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies"></Redirect>
+            <Redirect to="/not-found"></Redirect>
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
